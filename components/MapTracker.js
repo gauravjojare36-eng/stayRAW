4c611df0010be80155f29a5e3c6521bf829fbac1
gauravjojare36-eng <gauravjojare36@gmail.com>
2026-02-22 00:48:31 -0800

    Add MapTracker component with embedded Google Map

---
+++ components/MapTracker.js
@@ -0,0 +1,10 @@
+export default function MapTracker(){
+  return(
+    <iframe
+      width="100%"
+      height="400"
+      loading="lazy"
+      src="https://www.google.com/maps?q=18.5204,73.8567&output=embed">
+    </iframe>
+  )
+}
